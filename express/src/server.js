2585490fe88822b48e249d7c00359394d31a95b3
goast <goastie@goast.ai>
2023-11-12 21:14:02 +0000

    Fix for issue

--- express/src/server.js
+++ express/src/server.js
@@ -6,8 +6,7 @@ let users = [
     { id: 1, name: 'Alice', age: 25 },
     { id: 2, name: 'Bob', age: 30 },
     { id: 3, name: 'Charlie', age: 35 },
-];
-
+] || [];
 app.use(express.json());
 
 app.get('/users', (req, res) => {
@@ -15,14 +14,25 @@ app.get('/users', (req, res) => {
 });
 
 app.put('/users/:id', (req, res) => {
-    const userIndex = users.findIndex(u => u.id === req.params.id);
+    try {
+        const id = Number(req.params.id);
+        const userIndex = users.findIndex(u => u.id === id);
 
-    users[userIndex].name = req.body.name;
-    users[userIndex].age = req.body.age;
+        if (!req.body.name || !req.body.age) {
+            return res.status(400).send('Bad request: missing name or age');
+        }
+        if (userIndex === -1) {
+            return res.status(404).send('User not found');
+        }
 
-    res.json(users[userIndex]);
+        users[userIndex].name = req.body.name;
+        users[userIndex].age = req.body.age;
+
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('An unexpected error occurred');
+    }
 });
-
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
